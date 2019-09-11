5499b9973863c53b2d10ff2cecd530b569ba7391
dzabaj01 <dzanan.bajgoric@arm.com>
2019-09-11 17:09:50 +0200

    Adding a few more tests to the Stack unit tests.
    Change-Id: I5dd44bd138ee08d62f9049380f96d2d473e40f5e

--- data-structs-algorithms-java/src/test/java/com/toptalprep/StackTest.java
+++ data-structs-algorithms-java/src/test/java/com/toptalprep/StackTest.java
@@ -30,4 +30,32 @@ public class StackTest {
 		assertFalse(s.isEmpty());
 		assertFalse(s.isFull());
 	}
+	
+	/**
+	 * Asserts that empty and full flags are correct for a full stack.
+	 */
+	@Test
+	public void fullStackHasCorrectEmptyAndFullFlags() {
+		Stack s = new Stack(5);
+		s.push(5);
+		s.push(-12);
+		s.push(0);
+		s.push(100);
+		s.push(-1);
+		assertFalse(s.isEmpty());
+		assertTrue(s.isFull());
+	}
+	
+	/**
+	 * Asserts that the stack has correct empty and full flags after it
+	 * becomes empty.
+	 */
+	@Test
+	public void emptyAndFullFlagsAreCorectAfterStackBecomesEmpty() {
+		Stack s = new Stack(5);
+		s.push(7);
+		s.pop();
+		assertTrue(s.isEmpty());
+		assertFalse(s.isFull());
+	}
 }
