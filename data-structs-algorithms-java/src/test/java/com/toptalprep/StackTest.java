b3477bbe4cea018c1f99e92495adafcf37343ff0
dzabaj01 <dzanan.bajgoric@arm.com>
2019-09-11 18:41:35 +0200

    Completing the Stack unit tests.
    Change-Id: I74e07265be0cc13e45aed71a022dd5eb63cc6a77

--- data-structs-algorithms-java/src/test/java/com/toptalprep/StackTest.java
+++ data-structs-algorithms-java/src/test/java/com/toptalprep/StackTest.java
@@ -2,6 +2,7 @@ package com.toptalprep;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -57,5 +58,57 @@ public class StackTest {
 		s.pop();
 		assertTrue(s.isEmpty());
 		assertFalse(s.isFull());
+	}
+	
+	/**
+	 * Asserts that exception is thrown if push is called on a full stack.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void exceptionThrownOnPushToFullStack() {
+		Stack s = new Stack(5);
+		s.push(0);
+		s.push(-12);
+		s.push(56);
+		s.push(-345);
+		s.push(99);
+		
+		// Stack should be full at this point
+		assertTrue(s.isFull());
+		s.push(43);
+	}
+	
+	/**
+	 * Asserts that exception is thrown if pop is called on an empty stack.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void exceptionThrownOnPopFromEmptyStack() {
+		Stack s = new Stack(5);
+		s.pop();
+	}
+	
+	/**
+	 * Asserts that exception is thrown if peak is called on an empty stack.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void exceptionThrownOnPeakOnEmptyStack() {
+		Stack s = new Stack(5);
+		s.peak();
+	}
+	
+	/**
+	 * Tests push, pop and peak stack functionality.
+	 */
+	@Test
+	public void exercisePushPopPeak() {
+		Stack s = new Stack(10);
+		s.push(5);
+		assertEquals(s.peak(), 5);
+		s.push(45);
+		s.push(-34);
+		assertEquals(s.peak(), -34);
+		assertEquals(s.pop(), -34);
+		assertEquals(s.peak(), 45);
+		assertEquals(s.pop(), 45);
+		assertEquals(s.peak(), 5);
 	}
 }
