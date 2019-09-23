4f8d603a91a8df325210d7fadae1262f50cb486a
dzabaj01 <dzanan.bajgoric@arm.com>
2019-09-23 19:07:28 +0200

    ArithmeticExpressionParser class completed together with its unit tests.
    Change-Id: I1e090c05189aa11f1791c2f8af7b1cf1204e5bf1

--- coding-practice/java/programming-challenges/src/main/java/com/toptalprep/ArithmeticExpressionParser.java
+++ coding-practice/java/programming-challenges/src/main/java/com/toptalprep/ArithmeticExpressionParser.java
@@ -1,5 +1,7 @@
 package com.toptalprep;
 
+import java.util.Vector;
+
 import com.toptalprep.Stack;
 
 /**
@@ -11,10 +13,28 @@ import com.toptalprep.Stack;
  * 2 + 3 * 10 => 32
  * 2 * (5 + 10) => 30
  * (2 + 3) * (7 + 3) => 50
+ *
+ * The implementation assumes that there are no white-spaces
+ * between tokens. It also doesn't handle negative numbers so
+ * an expression -1+2 won't parse.
  */
 public class ArithmeticExpressionParser {
-    private static boolean isOperand(char c) {
-        return c != '+' && c != '-' && c != '*' && c != '/' && c != '(' && c != ')';
+	private enum TokenType {
+		NONE,
+		OPENING_PARENTHESES,
+		CLOSING_PARENTHESES,
+		OPERAND,
+		OPERATOR
+	}
+
+	/**
+	 * Whether a given char is a digit.
+	 *
+	 * @param c The char value.
+	 * @return Returns true if character is a digit, false otherwise.
+	 */
+    private static boolean isOperandChar(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private static boolean isOperator(char c) {
@@ -64,48 +84,145 @@ public class ArithmeticExpressionParser {
             }
         }
     }
+    
+    private static int calculate(int operand1, int operand2, char operator) throws Exception {
+    	switch (operator) {
+    	case '+':
+    		return operand1 + operand2;
+    	case '-':
+    		return operand1 - operand2;
+    	case '*':
+    		return operand1 * operand2;
+    	case '/':
+    		return operand1 / operand2;
+    	default:
+    		break;
+    	}
+    	throw new Exception("Unknown operator '" + operator + "'");
+    }
 
-    public static String convertToPostfix(String expr) throws Exception {
-        String postfix = "";
+    /**
+     * Converts infix expression to a postfix one. The tokens are stored in a
+     * vector to simplify support for multi-digit numbers.
+     *
+     * @param expr The arithmetic expression that is to be converted.
+     * @return A vector of tokens representing the postfix expression.
+     * @throws Exception
+     */
+    private static Vector<String> convertToPostfix(String expr) throws Exception {
+    	if (expr.isEmpty()) {
+    		throw new Exception("The expression is an empty string");
+    	}
+    	
+    	Vector<String> postfix = new Vector<>();
         Stack<Character> stack = new Stack<>(expr.length(), Character.class);
+        TokenType previous_token = TokenType.NONE;
 
         for (int i = 0; i < expr.length(); ++i) {
             char c = expr.charAt(i);
 
-            if (isOperand(c)) {
-                postfix += c;
+            if (isOperandChar(c)) {
+            	if (previous_token != TokenType.NONE && previous_token != TokenType.OPERATOR &&
+            			previous_token != TokenType.OPENING_PARENTHESES) {
+            		throw new Exception("Unexpected char '" + c + "' at position " + i);
+            	}
+            	
+            	// Read the entire operand
+            	String operand = "";
+            	for (; i < expr.length(); ++i) {
+            		c = expr.charAt(i);
+            		if (!isOperandChar(c)) {
+            			--i;
+            			break;
+            		}
+            		operand += c;
+            	}
+            	
+                postfix.add(operand);
+                previous_token = TokenType.OPERAND;
             }
             else if (c == '(') {
+            	if (previous_token != TokenType.NONE && previous_token != TokenType.OPENING_PARENTHESES &&
+            			previous_token != TokenType.OPERATOR) {
+            		throw new Exception("Unexpected char '" + c + "' at position " + i);
+            	}
+            	
                 stack.push(c);
+                previous_token = TokenType.OPENING_PARENTHESES;
             }
             else if (isOperator(c)) {
+            	if (previous_token != TokenType.OPERAND && previous_token != TokenType.CLOSING_PARENTHESES) {
+            		throw new Exception("Unexpected char '" + c + "' at position " + i);
+            	}
+            	
+            	// Pop all operators with greater or equal precedence from the stack, until either the
+            	// opening parentheses is reached or stack becomes empty
                 while (!stack.isEmpty() && stack.peak() != '(' && comparePrecedence(c, stack.peak()) <= 0) {
-                    postfix += stack.pop();
+                    postfix.add(stack.pop().toString());
                 }
+                
                 // Push the current operator to the stack
                 stack.push(c);
+                previous_token = TokenType.OPERATOR;
             }
             else if (c == ')') {
+            	if (previous_token != TokenType.OPERAND && previous_token != TokenType.CLOSING_PARENTHESES) {
+            		throw new Exception("Unexpected char '" + c + "' at position " + i);
+            	}
+            	
                 while (stack.peak() != '(') {
-                    postfix += stack.pop();
+                    postfix.add(stack.pop().toString());
+                    
+                    if (stack.isEmpty()) {
+                    	throw new Exception("Couldn't find the matching '(' parentheses");
+                    }
                 }
+                
                 // Pop the '('
                 stack.pop();
+                previous_token = TokenType.CLOSING_PARENTHESES;
             }
             else {
-                throw new Exception("Malformed expression");
+            	throw new Exception("Unexpected char '" + c + "' at position " + i);
             }
         }
-
+        
+        // Make sure the expression ends with an expected token
+        if (previous_token != TokenType.OPERAND && previous_token != TokenType.CLOSING_PARENTHESES) {
+        	throw new Exception("Unexpected char at the end of the expression");
+        }
+        
         // Pop all remaining operators from the stack
         while (!stack.isEmpty()) {
             if (!isOperator(stack.peak())) {
                 throw new Exception("Malformed expression");
             }
 
-            postfix += stack.pop();
+            postfix.add(stack.pop().toString());
         }
 
         return postfix;
+    }
+    
+    public static int evaluate(String expr) throws Exception {
+    	Vector<String> postfix = convertToPostfix(expr);
+    	
+    	// Iterate over the postfix array and push each operand onto a stack
+    	// When operator is reached apply it to two operands at the top of the 
+    	// stack (the two operands to the left of the operator).
+    	Stack<Integer> stack = new Stack<>(postfix.size(), Integer.class);
+    	for (String token: postfix) {
+    		if (isOperator(token.charAt(0))) {
+    			int operand2 = stack.pop();
+    			int operand1 = stack.pop();
+    			stack.push(calculate(operand1, operand2, token.charAt(0)));
+    		}
+    		else {
+    			stack.push(Integer.valueOf(token));
+    		}
+    	}
+    	
+    	// The results is at the top of the stack
+    	return stack.pop();
     }
 }
--- coding-practice/java/programming-challenges/src/test/java/com/toptalprep/ArithmeticExpressionParserTest.java
+++ coding-practice/java/programming-challenges/src/test/java/com/toptalprep/ArithmeticExpressionParserTest.java
@@ -1,29 +1,82 @@
 package com.toptalprep;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 /**
  * Unit tests for the ArithmeticExpressionParser class.
  */
 public class ArithmeticExpressionParserTest {
+	/**
+	 * Tests the parser with several well-formed expressions.
+	 *
+	 * @throws Exception
+	 */
     @Test
-    public void bla() throws Exception {
-        String[] expressions = {
-                "A+B-C",
-                "A*B/C",
-                "A+B*C",
-                "A*B+C",
-                "A*(B+C)",
-                "A*B+C*D",
-                "(A+B)*(C-D)",
-                "((A+B)*C)-D",
-                "A+B*(C-D/(E+F))",
-                "A+(B+C/D*E+F*G)"
+    public void testWellformedExpressions() throws Exception {
+    	class Pair {
+    		String m_expr;
+    		int m_expected_value;
+    		
+    		Pair(String expr, int value) {
+    			m_expr = expr;
+    			m_expected_value = value;
+    		}
+    	}
+    	
+        Pair[] expressions = {
+        		new Pair("1", 1),
+        		new Pair("23+3", 26),
+        		new Pair("(45)", 45),
+        		new Pair("((123))", 123),
+        		new Pair("(12-345)", -333),
+                new Pair("2+1-0", 3),
+                new Pair("43*6/3", 86),
+                new Pair("88+2*0099", 286),
+                new Pair("6*12+55", 127),
+                new Pair("32*(3+7)", 320),
+                new Pair("234*6-6*99", 810),
+                new Pair("(34+1)*(78-1234)", -40460),
+                new Pair("((6743+43)*12)-09", 81423),
+                new Pair("451+056*(31-54/(23+87))", 2187),
+                new Pair("31+(456+9876/34*56+1223*5)", 6607)
         };
 
-        for (String infix: expressions) {
-            System.out.print("Infix = " + infix + ", Postfix = ");
-            System.out.println(ArithmeticExpressionParser.convertToPostfix(infix));
+        for (Pair expr: expressions) {
+            assertEquals(expr.m_expected_value, ArithmeticExpressionParser.evaluate(expr.m_expr));
         }
+    }
+    
+    /**
+     * Tests parser's behavior when malformed expressions are provided.
+     */
+    @Test
+    public void testMalformedExpressions() {
+    	String[] malformed_expressions = {
+    			"",
+    			"(",
+    			")",
+    			"()",
+    			"(45+98",
+    			"098+/45",
+    			"*32+65",
+    			"980+12*56)",
+    			"456*(160+56)/",
+    			"(56+120)*((91-084)",
+    			"((1+34())*53)-91",
+    			"(34+98761)67"
+    	};
+    	
+    	for (String expr: malformed_expressions) {
+    		try {
+    			ArithmeticExpressionParser.evaluate(expr);
+    			assertTrue("Exception has not been thrown", false);
+    		}
+    		catch(Exception e) {
+    			
+    		}
+    	}
     }
 }
