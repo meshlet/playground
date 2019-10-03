783870871836e6e68a49b20375276861eb617542
dzabaj01 <dzanan.bajgoric@arm.com>
2019-10-03 14:41:35 +0200

    Introducing DoublyLinkedList class with its unit tests.
    Change-Id: I20d5e8669634ac7b21150453ce7f2e82d9ba4181

---
+++ coding-practice/c#/data-structures-algorithms-test/Tests/DoublyLinkedListTest.cs
@@ -0,0 +1,213 @@
+﻿using NUnit.Framework;
+using datastructuresalgorithms;
+using System;
+
+namespace datastructuresalgorithmstest
+{
+    /**
+     * Unit tests for the DoublyLinkedList
+     */
+    [TestFixture()]
+    public class DoublyLinkedListTest
+    {
+        /**
+         * Asserts that an empty list has correctly set empty flag.
+         */
+        [Test()]
+        public void EmptyFlagTrueInEmptyLinkedList()
+        {
+            var list = new DoublyLinkedList<int>();
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        /**
+         * Asserts that a non-empty list has correctly set empty flag.
+         */
+        [Test()]
+        public void EmptyFlagFalseInNonEmptyLinkedList()
+        {
+            var list = new DoublyLinkedList<int>();
+            list.InsertLast(4);
+            Assert.IsFalse(list.IsEmpty());
+        }
+
+        /**
+         * Exercises the InsertFirst method.
+         */
+        [Test()]
+        public void TestInsertFirst()
+        {
+            var list = new DoublyLinkedList<int>();
+            int[] values = { -5, 3, 1, 4, 4, 18, -20, 7, 12 };
+
+            // Push all items from 'values' to the list with PushFront
+            foreach (int i in values)
+            {
+                list.InsertFirst(i);
+            }
+
+            // Iterate through the list and verify that elements are
+            // exactly in reversed order compared to 'values' array
+            Assert.AreEqual(values.Length, list.Size);
+            int index = values.Length - 1;
+
+            foreach (int i in list)
+            {
+                Assert.AreEqual(values[index--], i);
+            }
+        }
+
+        /**
+         * Exercises the InsertLast method.
+         */
+        [Test()]
+        public void TestInsertLast()
+        {
+            var list = new DoublyLinkedList<int>();
+            int[] values = { -5, 3, 1, 4, 4, 18, -20, 7, 12 };
+
+            // Push all items from 'values' to the list with PushBack
+            foreach (int i in values)
+            {
+                list.InsertLast(i);
+            }
+
+            // Iterate through the list and verify that elements are
+            // in the same order as in 'values' array
+            Assert.AreEqual(values.Length, list.Size);
+            int index = 0;
+
+            foreach (int i in list)
+            {
+                Assert.AreEqual(values[index++], i);
+            }
+        }
+
+        /**
+         * Tests the RemoveFirst method.
+         */
+        [Test()]
+        public void TestRemoveFirst()
+        {
+            var list = new DoublyLinkedList<int>();
+            int[] values = { -4, 3, 5, -6, 3, 5, -20, 0 };
+
+            // Push the items from 'values' array to the list
+            foreach (int val in values)
+            {
+                list.InsertLast(val);
+            }
+
+            // Repeatedly call PopFront until list gets empty
+            int i = 0;
+            int expected_size = values.Length;
+            while (!list.IsEmpty())
+            {
+                Assert.AreEqual(expected_size--, list.Size);
+                Assert.AreEqual(values[i++], list.RemoveFirst());
+            }
+        }
+
+        /**
+         * Tests the RemoveLast method.
+         */
+        [Test()]
+        public void TestPopBack()
+        {
+            var list = new DoublyLinkedList<int>();
+            int[] values = { -4, 3, 5, -6, 3, 5, -20, 0 };
+
+            // Push the items from 'values' array to the list
+            foreach (int val in values)
+            {
+                list.InsertLast(val);
+            }
+
+            // Repeatedly call PopBack until list gets empty
+            int i = values.Length - 1;
+            int expected_size = values.Length;
+            while (!list.IsEmpty())
+            {
+                Assert.AreEqual(expected_size--, list.Size);
+                Assert.AreEqual(values[i--], list.RemoveLast());
+            }
+        }
+
+        /**
+         * Tests the Remove method.
+         */
+        [Test()]
+        public void TestRemove()
+        {
+            // The first array in the tuple represents the contents of
+            // the list before calling 'Remove', the second array is
+            // the list after calling 'Remove'. The third element of
+            // the tuple represents the value to be removed from the
+            // list.
+            Tuple<int[], int[], int>[] test_vectors =
+            {
+                new Tuple<int[], int[], int>(
+                    new int[] { },
+                    new int[] { },
+                    5),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { -2, 9, 0 },
+                    new int[] { -2, 9, 0 },
+                    6),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { 6 },
+                    new int[] { },
+                    6),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { -2, 5, 0 },
+                    new int[] { -2, 0 },
+                    5),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { 4, 4, 4, 4, 4 },
+                    new int[] { 4, 4, 4, 4 },
+                    4),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { -3, 5, 6, 7, -3 },
+                    new int[] { 5, 6, 7, -3 },
+                    -3),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { 10, 2, 5, 6, 2, 2, 2 },
+                    new int[] { 10, 5, 6, 2, 2, 2 },
+                    2),
+
+                new Tuple<int[], int[], int>(
+                    new int[] { -1, 0, 0, 5, -1, 9 },
+                    new int[] { 0, 0, 5, -1, 9 },
+                    -1),
+            };
+
+            foreach (Tuple<int[], int[], int> test_vector in test_vectors)
+            {
+                var list = new DoublyLinkedList<int>();
+
+                // Populate the list with the items from Tuple.Item1
+                foreach (int value in test_vector.Item1)
+                {
+                    list.InsertLast(value);
+                }
+
+                // Remove Tuple.Item3 value from the list
+                list.Remove(test_vector.Item3);
+
+                // Verify that the contents of the list is equal to Tuple.Item2
+                Assert.AreEqual(test_vector.Item2.Length, list.Size);
+                int index = 0;
+                foreach (int value in list)
+                {
+                    Assert.AreEqual(test_vector.Item2[index++], value);
+                }
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/data-structures-algorithms/Sources/DoublyLinkedList.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace datastructuresalgorithms
+{
+    /**
+     * Implements a doubly linked list.
+     */
+    public class DoublyLinkedList<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private class Link
+        {
+            public T Data { get; set; }
+            public Link Left { get; set; }
+            public Link Right { get; set; }
+
+            public Link(T data)
+            {
+                Data = data;
+                Left = Right = null;
+            }
+        }
+
+        Link head;
+        Link tail;
+
+        public uint Size { get; private set; }
+
+        public DoublyLinkedList()
+        {
+            head = tail = null;
+            Size = 0;
+        }
+
+        /**
+         * Inserts the item to the beginning of the list.
+         */
+        public void InsertFirst(T value)
+        {
+            Link link = new Link(value);
+            link.Right = head;
+
+            if (head != null)
+            {
+                head.Left = link;
+            }
+            else
+            {
+                // If this is the very first item, set tail to point to it
+                tail = link;
+            }
+
+            head = link;
+            ++Size;
+        }
+
+        /**
+         * Inserts the item to the end of the list.
+         */
+        public void InsertLast(T value)
+        {
+            Link link = new Link(value);
+            link.Left = tail;
+
+            if (tail != null)
+            {
+                tail.Right = link;
+            }
+            else
+            {
+                // If this is the very first item, set head to point to it
+                head = link;
+            }
+
+            tail = link;
+            ++Size;
+        }
+
+        /**
+         * Inserts the item right after the given value. If the list
+         * doesn't contain the specified value the list is left unchanged.
+         *
+         * If the list contains multiple items with the key value, the new
+         * item is inserted after the first occurence.        
+         *
+         * @return true if the value was inserted, false otherwise.
+         */
+        public bool InsertAfter(T search_key, T value)
+        {
+            Link current = head;
+
+            while (current != null && search_key.CompareTo(current.Data) != 0)
+            {
+                current = current.Right;
+            }
+
+            if (current != null)
+            {
+                if (current == head)
+                {
+                    InsertFirst(value);
+                }
+                else if (current == tail)
+                {
+                    InsertLast(value);
+                }
+                else
+                {
+                    Link link = new Link(value);
+                    link.Right = current.Right;
+                    link.Left = current;
+                    current.Right.Left = link;
+                    current.Right = link;
+                    ++Size;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Removes the item from the beginning of the list.
+         *
+         * The behavior is undefined if the list is empty.
+         */
+        public T RemoveFirst()
+        {
+            T data = head.Data;
+            head = head.Right;
+
+            if (head != null)
+            {
+                // The list is not empty
+                head.Left = null;
+            }
+            else
+            {
+                // The list is empty, reset the tail to null as well
+                tail = null;
+            }
+
+            --Size;
+            return data;
+        }
+
+        /**
+         * Removes the item from the end of the list.
+         *
+         * The behavior is undefiend if the list is empty.
+         */
+        public T RemoveLast()
+        {
+            T data = tail.Data;
+            tail = tail.Left;
+
+            if (tail != null)
+            {
+                // The list is not empty
+                tail.Right = null;
+            }
+            else
+            {
+                // The list is empty, reset the head to null as well
+                head = null;
+            }
+
+            --Size;
+            return data;
+        }
+
+        /**
+         * Removes the first item with the given value. If the list doesn't
+         * contain the value the list is left unmodified.
+         *
+         * @return true if the item has been removed, false otherwise.
+         */
+        public bool Remove(T value)
+        {
+            Link current = head;
+
+            while (current != null && value.CompareTo(current.Data) != 0)
+            {
+                current = current.Right;
+            }
+
+            if (current != null)
+            {
+                if (current == head)
+                {
+                    RemoveFirst();
+                }
+                else if (current == tail)
+                {
+                    RemoveLast();
+                }
+                else
+                {
+                    current.Left.Right = current.Right;
+                    current.Right.Left = current.Left;
+                    --Size;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Peaks the first item in the list without removing it.
+         *
+         * Behavior is undefined if the list is empty.
+         */
+        public T PeakFirst()
+        {
+            return head.Data;
+        }
+
+        /**
+         * Peaks the last item in the list without removing it.
+         *
+         * Behavior is undefined if the list is empty.
+         */
+        public T PeakLast()
+        {
+            return tail.Data;
+        }
+
+        /**
+         * Clears the list;
+         */
+        public void Clear()
+        {
+            head = tail = null;
+            Size = 0;
+        }
+
+        /**
+         * Whether the list is empty or not.
+         */
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        /**
+         * Implements the IEnumerable<T> interface.
+         */
+        public IEnumerator<T> GetEnumerator()
+        {
+            Link current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Right;
+            }
+        }
+
+        /**
+         * Implements the non-generic IEnumerable interface.
+         */
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
