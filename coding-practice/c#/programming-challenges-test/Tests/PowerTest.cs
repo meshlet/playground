29dc243491a1cb03309bc6a93dbceccdf3a3330c
dzabaj01 <dzanan.bajgoric@arm.com>
2019-10-14 19:09:45 +0200

    Introducing Power class and its unit tests. The class implements unsigned base^exp function with O(log(exp)) time complexity.
    Change-Id: I4f1545fc597922e2dacb4711a55496b4d1070e86

---
+++ coding-practice/c#/programming-challenges-test/Tests/PowerTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using programmingchallenges;
+
+namespace programmingchallengestest
+{
+    /**
+     * Unit tests for the Power class.
+     */
+    [TestFixture()]
+    public class PowerTest
+    {
+        [Test()]
+        public void TestPowerCalculation()
+        {
+            /* Item 1 is the base, item 2 the exponent and item 3 expected result */
+            Tuple<uint, uint, uint>[] test_vectors =
+            {
+                new Tuple<uint, uint, uint>(0, 45, 0),
+                new Tuple<uint, uint, uint>(4, 0, 1),
+                new Tuple<uint, uint, uint>(2, 1, 2),
+                new Tuple<uint, uint, uint>(3, 2, 9),
+                new Tuple<uint, uint, uint>(3, 11, 177147),
+                new Tuple<uint, uint, uint>(2, 10, 1024),
+                new Tuple<uint, uint, uint>(1, 11, 1),
+                new Tuple<uint, uint, uint>(13, 1, 13),
+                new Tuple<uint, uint, uint>(17, 7, 410338673)
+            };
+
+            foreach (var test_vector in test_vectors)
+            {
+                Assert.AreEqual(test_vector.Item3, Power.calculate(test_vector.Item1, test_vector.Item2));
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/programming-challenges/Sources/Power.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace programmingchallenges
+{
+    /**
+     * Implements unsigned integer base^exp function that has O(log(exp))
+     * time complexity The class is not allowed to use the in-built pow
+     * function.    
+     */
+    public class Power
+    {
+        static public uint calculate(uint base_val, uint exp_val)
+        {
+            /* Handle x^0 as a special case */
+            if (exp_val == 0)
+            {
+                return 1;
+            }
+
+            return pow_internal(base_val, exp_val);
+        }
+
+        static private uint pow_internal(uint current_val, uint current_exp)
+        {
+            if (current_exp == 1)
+            {
+                return current_val;
+            }
+
+            uint tmp = pow_internal(current_val * current_val, current_exp / 2);
+            return (current_exp % 2 == 0) ? tmp : tmp * current_val;
+        }
+    }
+}
