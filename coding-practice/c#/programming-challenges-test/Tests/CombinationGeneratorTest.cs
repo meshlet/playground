9d6705af1b47afc21136cae075579dc878308c62
dzabaj01 <dzanan.bajgoric@arm.com>
2019-10-18 12:35:36 +0200

    Introducing CombinationGenerator class and its unit tests.
    The class implements an algorithm that will generate all combinations
    of a given length L given an input string with N characters.
    
    Change-Id: I0be045e86da2615abe2c1bd249be9113ce329a0a

---
+++ coding-practice/c#/programming-challenges-test/Tests/CombinationGeneratorTest.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using programmingchallenges;
+
+namespace programmingchallengestest
+{
+    /**
+     * Unit tests for the CombinationGenerator class.
+     */
+    [TestFixture()]
+    public class CombinationGeneratorTest
+    {
+        [Test()]
+        public void TestCombinationGenerator()
+        {
+            // Item1 is the input string whose characters will be used to
+            // generate combinations, item2 is the desired combination size
+            // and item3 is the array of combinations expected to be generated.
+            Tuple<string, uint, string[]>[] test_vectors =
+            {
+                new Tuple<string, uint, string[]>(
+                    "ABC",
+                    0,
+                    new string[] { }),
+
+                new Tuple<string, uint, string[]>(
+                    "ABCDE",
+                    6,
+                    new string[] { }),
+
+                new Tuple<string, uint, string[]>(
+                    "ABCDE",
+                    3,
+                    new string[] { "ABC", "ABD", "ABE", "ACD", "ACE", "ADE", "BCD", "BCE", "BDE", "CDE" }),
+
+                new Tuple<string, uint, string[]>(
+                    "ABCDE",
+                    1,
+                    new string[] { "A", "B", "C", "D", "E" }),
+
+                new Tuple<string, uint, string[]>(
+                    "ABCDE",
+                    2,
+                    new string[] { "AB", "AC", "AD", "AE", "BC", "BD", "BE", "CD", "CE", "DE" }),
+
+                new Tuple<string, uint, string[]>(
+                    "ABCDE",
+                    4,
+                    new string[] { "ABCD", "ABCE", "ABDE", "ACDE", "BCDE" })
+            };
+
+            foreach (var test_vector in test_vectors)
+            {
+                List<string> combinations = new List<string>();
+                CombinationGenerator.Run(test_vector.Item1, test_vector.Item2, combinations);
+
+                Assert.AreEqual(test_vector.Item3.Length, combinations.Count);
+
+                Array.Sort(test_vector.Item3);
+                combinations.Sort();
+
+                int i = 0;
+                foreach (var combination in test_vector.Item3)
+                {
+                    Assert.AreEqual(combination, combinations[i++]);
+                }
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/programming-challenges/Sources/CombinationGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace programmingchallenges
+{
+    /**
+     * Given an input string with N characters, generates all the
+     * character combinations of desired length L. For example for
+     * input string ABCDE and desired combination length 3 the
+     * algorithm will produce:
+     *
+     * ABC, ABD, ABE, ACD, ACE, ADE, BCD, BCE, BDE, CDE
+     */
+    public class CombinationGenerator
+    {
+        public static void Run(string input, uint size, List<string> combinations)
+        {
+            Debug.Assert(combinations != null);
+
+            // Return immediatelly if requested combination size is 0 or the
+            // number of items in the input array is smaller than size.
+            if (size == 0 || input.Length < size)
+            {
+                return;
+            }
+
+            RunHelper("", input, size, combinations);
+        }
+
+        private static void RunHelper(string prefix, string input, uint size, List<string> combinations)
+        {
+            if (prefix.Length == size)
+            {
+                combinations.Add(prefix);
+                return;
+            }
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                // Break the loop if we know we can't reach the desired size
+                if (prefix.Length + input.Length - i >= size)
+                {
+                    RunHelper(prefix + input[i], input.Substring(i + 1), size, combinations);
+                }
+            }
+        }
+    }
+}
