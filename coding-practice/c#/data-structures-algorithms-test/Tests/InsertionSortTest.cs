888aef8e5a1d643b8d0544f9222cffbd584c4da4
dzabaj01 <dzanan.bajgoric@arm.com>
2019-10-18 16:25:37 +0200

    Introducing InsertionSort class with its unit tests.
    The class implements the classic insertion sort algorithm.
    
    Change-Id: Ic3c97bd99903549381b9c2f60b75562fbfe5a73e

---
+++ coding-practice/c#/data-structures-algorithms-test/Tests/InsertionSortTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using datastructuresalgorithms;
+
+namespace datastructuresalgorithmstest
+{
+    /**
+     * Unit tests for the InsertionSort class.
+     */
+    [TestFixture()]
+    public class InsertionSortTest
+    {
+        [Test()]
+        public void TestInsertionSort()
+        {
+            int[][] test_arrays =
+            {
+                new int[] { },
+                new int[] { 3 },
+                new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1 },
+                new int[] { 2, 5, 6, 8, 10, 99, -56 },
+                new int[] { -1, 5, -5, -3, 5, 9, 3, 0, -34, 19, 87, 13 },
+                new int[] { 10, 2, 5, 11, 3, 10, 7, -5, -2, 4, 17, -7 }
+            };
+
+            foreach (var test_array in test_arrays)
+            {
+                int[] tmp_array = new int[test_array.Length];
+                test_array.CopyTo(tmp_array, 0);
+
+                // Sort the tmp_array using in-built sort method
+                Array.Sort(tmp_array);
+
+                // Sort the test_array using custom MergeSort implementation
+                InsertionSort<int>.Sort(test_array);
+                Assert.True(Enumerable.SequenceEqual(tmp_array, test_array));
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/data-structures-algorithms/Sources/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace datastructuresalgorithms
+{
+    /**
+     * Implements the InsertionSort algorithm.
+     */
+    public class InsertionSort<T> where T : IComparable<T>
+    {
+        public static void Sort(T[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                T tmp = array[i];
+                int j = i - 1;
+                for (; j >= 0 && tmp.CompareTo(array[j]) < 0; --j)
+                {
+                    array[j + 1] = array[j];
+                }
+
+                // Put the current element in its place
+                array[j + 1] = tmp;
+            }
+        }
+    }
+}
