cc65d64a1eabf880fa1c1ef64ad62aae78dec1b7
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2019-10-27 01:15:36 +0200

    Introducing ArrayPartitioner class with its unit tests.
    The class implements array partitioning algorithm very similar
    to the Hoare partitioning scheme used in some QuickSort
    implementations. The partitioning is done so that the left
    partition will only contain values lower than the given pivot
    value and the right partition will only contain values greater
    or equal to the pivot.

---
+++ coding-practice/c#/programming-challenges-test/Tests/ArrayPartitionerTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using NUnit.Framework;
+using programmingchallenges;
+
+namespace programmingchallengestest
+{
+    /**
+     * Unit tests for the Partitioner class.
+     */
+    [TestFixture()]
+    public class ArrayPartitionerTest
+    {
+        [Test()]
+        public void TestArrayPartitioner()
+        {
+            // Item1 is the input array, item2 is the pivot value, item3 is the
+            // partitioned array and item4 is the return value of the partitioning
+            // method (the index to the first element of the right portion of the
+            // array - values greater or equal to the pivot.
+            Tuple<int[], int, int[], int>[] test_vectors =
+            {
+                new Tuple<int[], int, int[], int>(
+                    new int[] { },
+                    9,
+                    new int[] { },
+                    -1),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 4 },
+                    2,
+                    new int[] { 4 },
+                    0),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 4 },
+                    4,
+                    new int[] { 4 },
+                    0),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 4 },
+                    6,
+                    new int[] { 4 },
+                    1),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 33, 0, -1, 5, 10, 3, 7, 11, -5, -2, 4, 13 },
+                    7,
+                    new int[] { 4, 0, -1, 5, -2, 3, -5, 11, 7, 10, 33, 13 },
+                    7),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 33, 0, -1, 5, 10, 7, 8, 11, -5, -2, 4, 13 },
+                    7,
+                    new int[] { 4, 0, -1, 5, -2, -5, 8, 11, 7, 10, 33, 13 },
+                    6),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+                    9,
+                    new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9 },
+                    0),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 5, 5, 5, 5, 5, 5, 5 },
+                    9,
+                    new int[] { 5, 5, 5, 5, 5, 5, 5 },
+                    7),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { -2, 5, 3, 10, 4, -7, 3, 15 },
+                    15,
+                    new int[] { -2, 5, 3, 10, 4, -7, 3, 15 },
+                    7),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { -5, 34, 18, 0, 1, -5, 9, 13, 19, -18, 0, 45 },
+                    10,
+                    new int[] { -5, 0, 1, -5, 9, -18, 0, 34, 18, 13, 19, 45 },
+                    7),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 0, -4, 10, 7, -6, 0, 37, 14, 198, -45, 0, -9, 9, 13, 0 },
+                    0,
+                    new int[] { -4, -6, -45, -9, 0, 10, 7, 0, 37, 14, 198, 0, 9, 13, 0 },
+                    4),
+
+                new Tuple<int[], int, int[], int>(
+                    new int[] { 17, 10, 21, -50, 50, 9, 17, -5, 25, 0, 100, -1, 101, 8, 80, -3, 33, 4 },
+                    15,
+                    new int[] { 10, -50, 9, -5, 0, -1, 8, -3, 4, 17, 21, 50, 17, 25, 100, 101, 80, 33 },
+                    9)
+            };
+
+            foreach (var test_vector in test_vectors)
+            {
+                int[] array = test_vector.Item1;
+                int pivot = test_vector.Item2;
+                int[] ref_array = test_vector.Item3;
+
+                int right_partition_start = ArrayPartitioner.Partition(array, pivot);
+                Assert.AreEqual(test_vector.Item4, right_partition_start);
+                Assert.AreEqual(ref_array.Length, array.Length);
+
+                // Check the contents of paritions only for non-empty test arrays
+                if (right_partition_start != -1)
+                {
+                    // Sort the left and right partitions of the ref_array independently
+                    Array.Sort(ref_array, 0, right_partition_start);
+                    Array.Sort(ref_array, right_partition_start, ref_array.Length - right_partition_start);
+
+                    // Sort the left and right partitions of the array independently
+                    Array.Sort(array, 0, right_partition_start);
+                    Array.Sort(array, right_partition_start, array.Length - right_partition_start);
+
+                    // Compare the left partition of the reference array to the left parition of the
+                    // actual array
+                    for (int i = 0; i < right_partition_start - 1; ++i)
+                    {
+                        Assert.AreEqual(ref_array[i], array[i]);
+                    }
+
+                    // Compare the right partition of the reference array to the right partition of the
+                    // actual array
+                    for (int i = right_partition_start; i < ref_array.Length; ++i)
+                    {
+                        Assert.AreEqual(ref_array[i], array[i]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/programming-challenges/Sources/ArrayPartitioner.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace programmingchallenges
+{
+    public class ArrayPartitioner
+    {
+        /**
+         * Partitions an array in two partitions given the pivot value. The
+         * lgorithm will rearrange the array in such a way that its left partition
+         * contains only values lower than pivot, and the right partition contains
+         * only values larger or equal to the pivot. There is no guarantees about
+         * the ordering of values in either partition.
+         *
+         * For input array: { 33, 0, -1, 5, 10, 7, 8, 11, -5, -2, 4, 13 }
+         * algoritm produces:
+         * { 4, 0, -1, 5, -2, -5, 8, 11, 7, 10, 33, 13 } and returns 6 which is
+         * the index of the first element of the right partition of the array (
+         * values greater or equal to the pivot). If all values in the array
+         * are greater or equal to the pivot (none of the values are moved) the
+         * algorithm will return 0 (the start of the array is also the start of
+         * the right partition of the array). If all values in the array are lower
+         * than the pivot, the algorithm returns the Array.Length indicating that
+         * the right partition of the array is empty.
+         *
+         * @param[in,out] array  The array to partition.
+         * @param         pivot  The pivot value.
+         *
+         * @return The index of the first element in the right partition of the
+         *         array (values greater or equal to the pivot). -1 is returned
+         *         if the array is empty.        
+         */
+        public static int Partition<T>(T[] array, T pivot) where T : IComparable<T>
+        {
+            // Handle empty array as a special case
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+
+            int left_ptr = -1, right_ptr = array.Length;
+
+            while (true)
+            {
+                // Advance the left_ptr to the right until an element greater or equal to
+                // the pivot is found
+                do
+                {
+                    ++left_ptr;
+                } while (left_ptr < array.Length && array[left_ptr].CompareTo(pivot) < 0);
+
+                // Advance the right_ptr to the left until an element lower than the pivot
+                // is found
+                do
+                {
+                    --right_ptr;
+                } while (right_ptr >= 0 && array[right_ptr].CompareTo(pivot) >= 0);
+
+                if (left_ptr >= right_ptr)
+                {
+                    break;
+                }
+                else
+                {
+                    // Swap the array[left_ptr] and array[right_ptr] elements, so that array[left_ptr]
+                    // lands in the right partition of the array where values are >= pivot and
+                    // array[right_ptr] lands in the left partition of the array where values are < pivot.
+                    T tmp = array[left_ptr];
+                    array[left_ptr] = array[right_ptr];
+                    array[right_ptr] = tmp;
+                }
+            }
+
+            // right_ptr + 1 always points to the beginning of the right partition of the array
+            return right_ptr + 1;
+        }
+    }
+}
