a4dc9c5898200b049edfc4b0457af58165dfd3a0
dzabaj01 <dzanan.bajgoric@arm.com>
2019-11-04 19:24:56 +0100

    Introducing RadixSortDecimal class with its unit tests.
    The class implements the RadixSort algorithm that places
    items into buckets based on its decimal digits. The time
    complexity of the algorithm is O(N*W) where N is the
    number of items in the array and W is maximal numbef of
    digits of any item in the array. The algorithm also
    handles arrays with negative numbers by initially
    partitioning the array around 0 and then separately
    radix sorting the left (non-positive) and right (non-
    negative) parition.
    
    Change-Id: Ic1d4e6688899e83bfbd818b0f4cfd977ac704972

---
+++ coding-practice/c#/data-structures-algorithms-test/Tests/RadixSortDecimalTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using datastructuresalgorithms;
+
+namespace datastructuresalgorithmstest
+{
+    /**
+     * Unit tests for the QuickSortBasic.
+     */
+    [TestFixture()]
+    public class RadixSortDecimalTest
+    {
+        [Test()]
+        public void TestRadixSortDecimal()
+        {
+            long[][] test_arrays =
+            {
+                new long[] { },
+                new long[] { 3 },
+                new long[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1 },
+                new long[] { 2, 5, 6, 8, 10, 99, 56 },
+                new long[] { -1, 5, -5, -3, 5, 9, 3, 0, -34, 19, 87, 13 },
+                new long[] { 10, 2, 5, 11, 3, 10, 7, -5, -2, 4, 17, -7 },
+                new long[] { 33, 0, -1, 5, 10, 3, 7, 11, -5, -2, 4, 13 },
+                new long[] { 33, 0, -1, 5, 10, 7, 8, 11, -5, -2, 4, 13 },
+                new long[] { 8, 10, 12, 7, 16, 15, 17, 13, 11, 23, 24, 31 },
+                new long[] { 17, 10, 21, -50, 50, 9, 17, -5, 25, 0, 100, -1, 101, 8, 80, -3, 33, 4 },
+                new long[] { 0, -4, 10, 7, -6, 0, 37, 14, 198, -45, 0, -9, 9, 13, 0 },
+                new long[] { -5, 34, 18, 0, 1, -5, 9, 13, 19, -18, 0, 45 },
+                new long[] { -2, 5, 3, 10, 4, -7, 3, 15 }
+            };
+
+            foreach (var test_array in test_arrays)
+            {
+                long[] tmp_array = new long[test_array.Length];
+                test_array.CopyTo(tmp_array, 0);
+
+                // Sort the tmp_array using in-built sort method
+                Array.Sort(tmp_array);
+
+                // Sort the test_array using custom RadixSortDecimal implementation
+                RadixSortDecimal.Sort(test_array);
+                Assert.True(Enumerable.SequenceEqual(tmp_array, test_array));
+            }
+        }
+    }
+}
---
+++ coding-practice/c#/data-structures-algorithms/Sources/RadixSortDecimal.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace datastructuresalgorithms
+{
+    /**
+     * Implements the radix sort algorithm where the items are placed into
+     * buckets based on the decimal digits. The algorithm starts with the
+     * least significant digit and progresses to the most significant one.
+     * The algorithm terminates when there are no more digits left in any
+     * of the array items. Hence the time complexity of the algorithm is
+     * O(N*W) where N is the number of array items and W is the maximal
+     * number of decimal digits of any array item.
+     */
+    public class RadixSortDecimal
+    {
+        public static void Sort(long[] array)
+        {
+            // Return right away if array is empty or has 1 element
+            if (array.Length <= 1)
+            {
+                return;
+            }
+
+            // Partition the array around 0
+            int non_negative_partition_start = PartitionAroundZero(array);
+
+            // Setup 10 buckets for each of the 10 decimal digits (from 0 to 9 or
+            // from -9 to 0). Buckets are setup here and not in SortInternal to
+            // avoid setting them up twice (when sorting the non-positive and
+            // non-negative partitions). Note that buckets are cleared by each
+            // call to SortInternal so one call to SortInternal won't affect
+            // the other.
+            DoublyLinkedList<long>[] buckets = new DoublyLinkedList<long>[10];
+            for (int i = 0; i < buckets.Length; ++i)
+            {
+                buckets[i] = new DoublyLinkedList<long>();
+            }
+
+            // Radix sort the non-positive (left) partition of the array
+            SortInternal(array, buckets, 0, non_negative_partition_start - 1, true);
+
+            // Radix sort the non-negative (right) partition of the array
+            SortInternal(array, buckets, non_negative_partition_start, array.Length - 1, false);
+        }
+
+        private static void SortInternal(
+            long[] array, DoublyLinkedList<long>[] buckets, int start, int end,
+            bool is_negative_partition)
+        {
+            // Return right away if partition is empty or has a single element
+            if (start >= end)
+            {
+                return;
+            }
+
+            long divisor = 1;
+            bool run = true;
+
+            // The values are usually placed in their buckets using the current
+            // digit as an index. This doesn't work for negative numbers though,
+            // as their digits will be negative integers. Hence, if we're currently
+            // sorting the partition with non-positive values, add '9' to the digit
+            // value to form the index into the bucket. For digit value of -9 this
+            // will result in index 0, and for digit 0 it will result in 9 which
+            // means that negative numbers are properly sorted in the bucket.
+            int bucket_offset = is_negative_partition ? 9 : 0;
+
+            while (run)
+            {
+                run = false;
+
+                // Iterate over the array and place items into the buckets according
+                // to the value of the current decimal digit
+                for (int i = start; i <= end; ++i)
+                {
+                    long tmp = array[i] / divisor;
+                    long digit = (tmp % 10);
+                    buckets[digit + bucket_offset].InsertLast(array[i]);
+
+                    // If this is the last decimal digit for all array items we'll
+                    // break the outer-most while loop
+                    run = run || (tmp > 9 || tmp < -9);
+                }
+
+                // Next iteration handles the next decimal digit
+                divisor *= 10;
+
+                // Copy buckets' contents back to the array. For example, if array
+                // contains items 36 and 37, the first iteration of the other most
+                // do loop will place 36 into bucket 'i' and 37 into bucket 'i+1'.
+                // The following for loop will copy these items back to the original
+                // array in the sorted order ([...,36,...,37,...]). In the next
+                // iteration of the outer most do loop, these items are placed in
+                // the same bucket in which they will keep their order. In other
+                // words, these sub-sorts are stable.
+                long array_index = start;
+                for (int i = 0; i < buckets.Length; ++i)
+                {
+                    while (!buckets[i].IsEmpty())
+                    {
+                        array[array_index++] = buckets[i].RemoveFirst();
+                    }
+                }
+            }
+        }
+
+        private static int PartitionAroundZero(long[] array)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            int left_ptr = -1;
+            int right_ptr = array.Length;
+
+            while (true)
+            {
+                // Increment the left_ptr until an element greater or equal
+                // to 0 is found or end of array is reached
+                while (left_ptr < end && array[++left_ptr] < 0)
+                {
+                }
+
+                // Decrement the right_ptr until an element lower or equal
+                // to 0 is found or start of array is reached
+                while (right_ptr > start && array[--right_ptr] > 0)
+                {
+                }
+
+                // If left_ptr and right_ptr have overlapped, return the left_ptr
+                // which points to the start of the right partition (values >= 0)
+                if (left_ptr >= right_ptr)
+                {
+                    return left_ptr;
+                }
+
+                // Swap elements at left_ptr and right_ptr
+                long tmp = array[left_ptr];
+                array[left_ptr] = array[right_ptr];
+                array[right_ptr] = tmp;
+            }
+        }
+    }
+}
