1b2efa30b010d0dae66fbf03f26acb318147cc08
dzabaj01 <dzanan.bajgoric@arm.com>
2020-02-18 18:55:22 +0100

    Updating prog-languages/javascript NPM module.
    - Adding js-samples.conf.js that defines the karma
      test runner module for the project.
    - Adding simple_test.js that contains to simple
      Jasmine tests.
    - package.json and package-lock.json updated with
      additional dependencies.
    
    Change-Id: I9f61ca540d932d7876ed830e105e5025f423a5f1

---
+++ prog-languages/javascript/test/simple_test.js
@@ -0,0 +1,8 @@
+describe("SampleTestSuite", function() {
+    it("will unconditionally pass", function() {
+        expect(true).toBe(true);
+    });
+    it("will unconditionally fail", function() {
+        expect(true).toBe(false);
+    });
+});
