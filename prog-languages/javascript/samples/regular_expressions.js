50e87516917f7766f070b94d20e8f9b8a7e86713
dzabaj01 <dzanan.bajgoric@arm.com>
2020-03-05 18:19:44 +0100

    Extending prog-languages/javascript/samples/regular_expressions.js
    Change-Id: Ia689acc21e1425a50e0129a21afbfe0dbbe31b44

--- prog-languages/javascript/samples/regular_expressions.js
+++ prog-languages/javascript/samples/regular_expressions.js
@@ -324,27 +324,6 @@ describe("Regular Expressions", function () {
         expect(regExp.test("ababab")).toBeTrue();
     });
 
-    it('illustrates back-references', function () {
-        // Matches one of 'a', 'b' or 'c' followed by x followed by
-        // whatever character matches the first capture (so either
-        // 'a', 'b' or 'c' depending on which character was matched).
-        let regExp1 = /([abc])x\1/;
-        expect(regExp1.test("abc")).toBeFalse();
-        expect(regExp1.test("axa")).toBeTrue();
-        expect(regExp1.test("axc")).toBeFalse();
-        expect(regExp1.test("cxc")).toBeTrue();
-
-        // Matches '<' followed by any number of alphanumeric characters
-        // followed by '>' followed by any number of any characters except
-        // the line terminators followed by '<' followed by the sequence
-        // that matched the first capture followed by '>'. This regexp
-        // basically matches an XML closing and opening tags.
-        let regExp2 = /<(\w+)>.*<\/\1>/;
-        expect(regExp2.test("<div>abc</div>")).toBeTrue();
-        expect(regExp2.test("abc")).toBeFalse();
-        expect(regExp2.test("<div+>a</div+>")).toBeFalse();
-    });
-
     it('illustrates finding all HTML elements with specified class name', function () {
         function prepareElements() {
             let parentDiv = document.createElement("div");
@@ -402,7 +381,14 @@ describe("Regular Expressions", function () {
         expect(findElementsWithClass(parentContainer, "class2").length).toBe(3);
     });
 
-    it('illustrates matching using local regular expressions', function () {
+    /**
+     * Local regular expressions (without the 'g' flag) terminate as soon as
+     * the character sequence is matched, even though there may be more such
+     * sequences in the string. For example, regular expression /test/ will
+     * match sequence 'test' exactly once. So for a candidate string
+     * 'test abc test a test' only the first 'test' sequence is matched.
+     */
+    it('illustrates matching/capturing using local regular expressions', function () {
         // A string representing the CSS transform property that translates
         // the element 50 pixels down the y-axis.
         let string = "transform: translateY(40px);";
@@ -419,6 +405,288 @@ describe("Regular Expressions", function () {
         let match = string.match(regexp);
         expect(match).not.toBeNull();
         expect(match.length).toBe(2);
+        expect(match[0]).toEqual("translateY(40px)");
         expect(match[1]).toEqual("40px");
+    });
+
+    /**
+     * Global regular expressions (with the 'g' flag) find all the match instances
+     * in the candidate string (as opposed to local expressions that terminate after
+     * the first match is found). For example, regular expression /test/g will match
+     * the all the 'test' sequences in the candidate string. So for the candidate
+     * string 'test abc test a test' all of the 'test' sequences are matched. Note,
+     * however, that capturing doesn't work out of the box with global expressions.
+     * The captures within matches are not returned by the String.match for global
+     * expressions. See the next sample on how to work around this.
+     */
+    it('illustrates difference between matching with local and global expressions', function () {
+        let htmlString = "<div class='test'><b>Hello</b><i>world!</i></div>";
+
+        // Match with the local regular expression. The expression matches
+        // zero or one '/' character, followed by one or more alphanumeric
+        // characters (tag name), followed by any character except '>' zero
+        // or more times, followed by the '>' character. As this is a local
+        // regular expression (no 'g' flag) only the first match instance
+        // is found and captures within that instance are returned.
+        let localMatch = htmlString.match(/<(\/?)(\w+)([^>]*)>/);
+        expect(localMatch.length).toBe(4);
+        expect(localMatch[0]).toEqual("<div class='test'>");
+        expect(localMatch[1]).toEqual("");
+        expect(localMatch[2]).toEqual("div");
+        expect(localMatch[3]).toEqual(" class='test'");
+
+        // Match with the global regular expression. The expression itself
+        // is identical to the previous one except the 'g' flag making this
+        // one global. String.match method returns only global matches in
+        // this case. In other words, only no matches are returned but not
+        // the captured within matches. See the next sample
+        let globalMatch = htmlString.match(/<(\/?)(\w+)([^>]*)>/g);
+        expect(globalMatch.length).toBe(6);
+        expect(globalMatch[0]).toEqual("<div class='test'>");
+        expect(globalMatch[1]).toEqual("<b>");
+        expect(globalMatch[2]).toEqual("</b>");
+        expect(globalMatch[3]).toEqual("<i>");
+        expect(globalMatch[4]).toEqual("</i>");
+        expect(globalMatch[5]).toEqual("</div>");
+    });
+
+    it('illustrates using RegExp.exec to do global search and capturing', function () {
+        let htmlString = "<div class='test'><b>Hello</b><i>world!</i></div>";
+
+        // Match with the local regular expression. The expression matches
+        // zero or one '/' character, followed by one or more alphanumeric
+        // characters (tag name), followed by any character except '>' zero
+        // or more times, followed by the '>' character. As this is global
+        // expression, only matches are returned by String.match but not
+        // the captures within them. To work around this, RegExp.exec
+        // method is used.
+        let regexp = /<(\/?)(\w+)([^>]*)>/g;
+
+        let actualResults = [];
+        let match;
+
+        // If expression is global the exec method becomes stateful. Each
+        // call will return the next match and captures within that match.
+        // This allows us to get all matches as well as the captures within
+        // each match.
+        while((match = regexp.exec(htmlString)) !== null) {
+            actualResults.push(...match);
+        }
+
+        let expectedResults = [
+            "<div class='test'>",
+            "",
+            "div",
+            " class='test'",
+            "<b>",
+            "",
+            "b",
+            "",
+            "</b>",
+            "/",
+            "b",
+            "",
+            "<i>",
+            "",
+            "i",
+            "",
+            "</i>",
+            "/",
+            "i",
+            "",
+            "</div>",
+            "/",
+            "div",
+            ""
+        ];
+
+        expect(actualResults).toEqual(expectedResults);
+    });
+
+    it('illustrates back-references', function () {
+        // Matches one of 'a', 'b' or 'c' followed by x followed by
+        // whatever character matches the first capture (so either
+        // 'a', 'b' or 'c' depending on which character was matched).
+        let regExp1 = /([abc])x\1/;
+        expect(regExp1.test("abc")).toBeFalse();
+        expect(regExp1.test("axa")).toBeTrue();
+        expect(regExp1.test("axc")).toBeFalse();
+        expect(regExp1.test("cxc")).toBeTrue();
+
+        // Matches '<' followed by one or more of alphanumeric characters
+        // followed by '>' followed by zero or more of any characters except
+        // the line terminators followed by '<' followed by the sequence
+        // that matched the first capture followed by '>'. This regexp
+        // basically matches an XML closing and opening tags.
+        let regExp2 = /<(\w+)>.*<\/\1>/;
+        expect(regExp2.test("<div>abc</div>")).toBeTrue();
+        expect(regExp2.test("abc")).toBeFalse();
+        expect(regExp2.test("<div+>a</div+>")).toBeFalse();
+
+        // Matches '<' followed by one or more alphanumeric characters (tag),
+        // followed by zero or more characters different than '>', followed
+        // by '>', followed by any number of characters which are not line
+        // terminators, followed by '<', followed by the very first capture
+        // within this match (the tag), followed by '>'. This is a global
+        // expression so RegExp.exec needs to be used to retrieve captures
+        // within matches.
+        let regExp3 = /<(\w+)([^>]*)>(.*)<\/\1>/g;
+
+        let htmlString = "<b class='hello'>Hello</b><i>world!</i>";
+        let actualResults = [];
+        let match;
+        while ((match = regExp3.exec(htmlString)) !== null) {
+            actualResults.push(...match);
+        }
+
+        let expectedResults = [
+            "<b class='hello'>Hello</b>",
+            "b",
+            " class='hello'",
+            "Hello",
+            "<i>world!</i>",
+            "i",
+            "",
+            "world!"
+        ];
+
+        expect(actualResults).toEqual(expectedResults);
+    });
+
+    it('illustrates using String.replace with regular expressions', function () {
+        // Replaces all uppercase letters in a string with letter 'X'
+        let regExp1 = /[A-Z]/g;
+        expect("abCdeGhIjklmOPQr".replace(regExp1, "X")).toEqual("abXdeXhXjklmXXXr");
+
+        // Converts the camelCase into dashed notation. The replace method
+        // applies the regexp to the string and replaces each match with
+        // the method's second argument. Note that the second argument
+        // can refer to the captures using $1, $2 and so on. The capture
+        // is always relative to the current match for global expressions.
+        let regEx2 = /([A-Z])/g;
+        expect("fontFamily".replace(regEx2, "-$1").toLowerCase()).toEqual("font-family");
+        expect("longCamelCaseStringThatWillBeConvertedToDashedNotation".replace(regEx2, "-$1").toLowerCase())
+            .toEqual("long-camel-case-string-that-will-be-converted-to-dashed-notation");
+    });
+
+    /**
+     * When String.replace is invoked with a function as its second argument,
+     * it invokes that function for each match. The arguments passed to that
+     * function depend on String.replace method's first argument. If regular
+     * expression is its first argument, then the following arguments are
+     * passed to the replacement function:
+     *
+     * - the full text of the match
+     * - the captures of the match, one argument for each
+     * - the index of the match within the original string
+     * - the source string
+     *
+     * The return value of the replacement function replaces the matched
+     * sequence in the original string.
+     */
+    it('illustrates using String.replace with regexp and replacement functions', function () {
+        // Converts dashed notation into camel case
+        expect(
+            "long-string-in-dashed-notation-that-will-be-converted-into-camel-case"
+                .replace(/-(\w)/g, (entireMatch, letter) => {
+                    return letter.toUpperCase();
+                })
+        ).toEqual("longStringInDashedNotationThatWillBeConvertedIntoCamelCase");
+
+        // The following shows how String.replace can be utilized a means of string
+        // traversal. We want to convert a query string of the
+        // 'abc=1&abc=2&foo=a&mix=23&foo=8&abc=p' into a more readable format
+        // 'abc=1,2,p&foo=a,8&mix=23'.
+        function compressQuery(query) {
+            let keys = new Map();
+
+            query.replace(
+                /(\w+)=(\w*)/g,
+                (match, key, value) => {
+                    if (keys.has(key)) {
+                        keys.set(key, keys.get(key) + "," + value);
+                    }
+                    else {
+                        keys.set(key, value);
+                    }
+
+                    // We don't care about the replacement string so simply return an
+                    // empty string. The intention is to use the String.replace function
+                    // as means of iterating over all matches
+                    return "";
+                }
+            );
+
+            let results = [];
+            for (let mapping of keys) {
+                results.push(mapping[0] + "=" + mapping[1]);
+            }
+            return results.join("&");
+        }
+
+        expect(compressQuery("abc=1&abc=2&foo=a&mix=23&foo=8&abc=p")).toEqual("abc=1,2,p&foo=a,8&mix=23");
+        expect(compressQuery("abc=&foo=12&foo=ty")).toEqual("abc=&foo=12,ty");
+    });
+
+    it('illustrates non-capturing groups', function () {
+        // Matches one or more 'prefix-' sequences followed by sequence 'name'.
+        // We'd like to capture everything that comes before 'name' (so all the
+        // 'prefix-' sequences), however parentheses must be placed around the
+        // 'prefix-' to apply the + operator to entire sequence. This creates
+        // the capture which will be returned by String.match
+        let regExp1 = /((prefix-)+)name/;
+        expect([..."prefix-name".match(regExp1)]).toEqual(["prefix-name", "prefix-", "prefix-"]);
+        expect([..."prefix-prefix-prefix-name".match(regExp1)])
+            .toEqual(["prefix-prefix-prefix-name", "prefix-prefix-prefix-", "prefix-"]);
+
+        // We can instruct the regexp engine not to capture a group by adding '?:'
+        // after the opening '(' parentheses. The following regexp is similar to
+        // the one above, however this one won't capture the inner 'prefix-' group.
+        // It will only capture the entire prefix (possibly made of multiple 'prefix-'
+        // sequences).
+        let regExp2 = /((?:prefix-)+)name/;
+        expect([..."prefix-name".match(regExp2)]).toEqual(["prefix-name", "prefix-"]);
+        expect([..."prefix-prefix-prefix-name".match(regExp2)])
+            .toEqual(["prefix-prefix-prefix-name", "prefix-prefix-prefix-"]);
+    });
+
+    it('illustrates matching all characters including newlines', function () {
+        // The following regex matches all characters except the line terminators
+        let regExp1 = /(.*)/;
+        expect("First line\nSecond line".match(regExp1)[0]).toEqual("First line");
+
+        // The following regex matches either a whitespace character or any other
+        // character except whitespace zero or more times. In other words, it matches
+        // any possible character zero or more times including the newline
+        let regExp2 = /([\s\S]*)/;
+        expect("First line\nSecond line".match(regExp2)[0]).toEqual("First line\nSecond line");
+
+        // The following regex matches either a whitespace character or a character
+        // that is not a line terminator, zero or more times. The union of these are
+        // all possible characters including newlines.
+        let regExp3 = /(?:.|\s)*/;
+        expect("First line\nSecond line".match(regExp3)[0]).toEqual("First line\nSecond line");
+    });
+
+    it('illustrates matching unicode characters', function () {
+        let unicodeText = "\u5FCD\u8005\u30D1\u30EF\u30FC";
+
+        // The following matches any common word character plus any unicode character
+        // above unicode character 128 (80 hexadecimal) plus the hyphen character,
+        // one or more times.
+        let regExp = /[\w\u0080-\uFFFF-]+/;
+        expect(regExp.test(unicodeText)).toBeTrue();
+    });
+
+    it('illustrates matching escaped characters', function () {
+        // Matches any alphanumeric character OR '\' followed by any character
+        // except the line terminators, one or more times. The sequence must
+        // encompass the entire candidate string to be matched.
+        let regExp = /^(?:\w|\\.)+$/;
+        expect(regExp.test("formUpdate")).toBeTrue();
+        expect(regExp.test("form\\.update\\.whatever")).toBeTrue();
+        expect(regExp.test("form\\:update")).toBeTrue();
+        expect(regExp.test("\\f\\o\\r\\m\\u\\p\\d\\a\\t\\e")).toBeTrue();
+        expect(regExp.test("form:update")).toBeFalse();
     });
 });
