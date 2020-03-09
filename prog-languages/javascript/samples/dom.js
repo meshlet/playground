fd2748c9972f39827b4decc7fc816e988b6fbff5
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2020-03-10 00:57:00 +0100

    Extending prog-languages/javascript/samples/dom.js samples.
    Adding samples that illustrate the offsetWidth and offsetHeight
    properties used to determine the total amount of space an element
    occupies, including the width of visible content, scrollbars (if
    any, padding and border.

--- prog-languages/javascript/samples/dom.js
+++ prog-languages/javascript/samples/dom.js
@@ -191,4 +191,81 @@ describe("Document Object Model", function () {
         expect(computedStyles.getPropertyValue("border-style")).toEqual("dashed");
         expect(computedStyles.getPropertyValue("border-color")).toEqual("rgb(0, 255, 0)");
     });
+
+    it('illustrates the offsetWidth/offsetHeight (no scrollbar)', function () {
+        // Create the container DIV whose width and height are left unspecified.
+        // This makes sure that DIV will extend as much as it needs to hold its
+        // children.
+        let containerDiv = document.createElement("div");
+
+        // Set the display property to inline-block to make sure the DIV's width
+        // doesn't take all the space available (the behavior when display is set
+        // to block which is the default for DIVs) but instead only extends enough
+        // to hold its children.
+        containerDiv.style.display = "inline-block";
+        containerDiv.style.border = "5px solid black";
+        containerDiv.style.padding = "10px";
+
+        // Create the inner div with fixed dimensions
+        let innerDiv = document.createElement("div");
+        innerDiv.style.width = "300px";
+        innerDiv.style.height = "200px";
+
+        // Append the DIV to the DOM to make sure its dimensions are calculated
+        containerDiv.appendChild(innerDiv);
+        document.getElementsByTagName("body")[0].appendChild(containerDiv);
+
+        // offsetWidth/offsetHeight include the border width, padding, scrollbar (if
+        // any) and the width of the visible content. Note that no scrollbar is added
+        // to container DIV in this case as both its width and height automatically
+        // extend to accommodate its children.
+        // offsetWidth is be equal to:
+        // 300px (innerDiv width) + 20px (left and right border width) + 10px (left and
+        // right padding = 330px.
+        // offsetHeight is similarly equal to: 230px.
+        expect(containerDiv.offsetWidth).toBe(330);
+        expect(containerDiv.offsetHeight).toBe(230);
+    });
+
+    it('illustrates the offsetWidth/offsetHeight (with scrollbar)', function () {
+        // Create the container DIV with auto width and fixed height. That means
+        // that DIV's width grows to accommodate its children but its height
+        // remains fixed.
+        let containerDiv = document.createElement("div");
+
+        // Set the display property to inline-block to make sure the DIV's width
+        // doesn't take all the space available (the behavior when display is set
+        // to block which is the default for DIVs) but instead only extends enough
+        // to hold its children. Additionally, set overflow-y to auto so that
+        // vertical scrollbar is added to the DIV in case the content becomes to
+        // high to fit inside the DIV.
+        containerDiv.style.height = "50px";
+        containerDiv.style.display = "inline-block";
+        containerDiv.style.overflowY = "auto";
+        containerDiv.style.border = "5px solid black";
+        containerDiv.style.padding = "10px";
+
+        // Create the inner div with fixed dimensions
+        let innerDiv = document.createElement("div");
+        innerDiv.style.width = "300px";
+        innerDiv.style.height = "200px";
+
+        // Append the DIV to the DOM to make sure its dimensions are calculated
+        containerDiv.appendChild(innerDiv);
+        document.getElementsByTagName("body")[0].appendChild(containerDiv);
+
+        // offsetWidth/offsetHeight include the border width, padding, scrollbar (if
+        // any) and the width of the visible content. Note that vertical scrollbar is
+        // added in this case, so its width is included in the offsetWidth. Hence, the
+        // offset width will be greater than the 330 pixels because the scrollbar width
+        // is accounted for as well. offsetHeight will be:
+        // 50px (fixed height of the div) + 20px (top and bottom padding) + 10px (top
+        // and bottom border) = 80px.
+        expect(containerDiv.offsetWidth).toBeGreaterThan(330);
+        expect(containerDiv.offsetHeight).toBe(80);
+    });
+
+    // Add more samples that illustrate the rest of properties for obtaining element
+    // dimensions:
+    // https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
 });
