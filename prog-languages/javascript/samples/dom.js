f73740e135ddd108dfb20c98047504de421642d2
dzabaj01 <dzanan.bajgoric@arm.com>
2020-03-09 18:27:14 +0100

    Extending prog-languages/javascript/samples/dom.js samples.
    Change-Id: Ie863c0fe64e957643120471bdc0100d01d6be3a6

--- prog-languages/javascript/samples/dom.js
+++ prog-languages/javascript/samples/dom.js
@@ -115,4 +115,80 @@ describe("Document Object Model", function () {
         div.setAttribute("id", "div3");
         expect(div.id).toEqual("div3");
     });
+
+    it('illustrates the difference between in-page style sheet and inline style attribute', function () {
+        // Create a style element, add some CSS to it and append it to
+        // the head element
+        const css = "div#testDiv { font-size: 20px; border: 0 solid black; }";
+        const styleElem = document.createElement("style");
+        styleElem.appendChild(document.createTextNode(css));
+        document.getElementsByTagName("head")[0].appendChild(styleElem);
+
+        // Create a #testDiv within an invisible container div, define
+        // its 'style' attribute and add the container div to the body
+        // of the page
+        let containerDiv = document.createElement("div");
+        containerDiv.style.display = "none";
+        containerDiv.innerHTML = "<div id='testDiv' style='color:red;'></div>";
+        document.getElementsByTagName("body")[0].appendChild(containerDiv);
+
+        // Obtain the reference to #testDiv div
+        let testDiv = document.getElementById("testDiv");
+        expect(testDiv).not.toBeNull();
+
+        // Assert that inline style attribute is respected
+        expect(testDiv.style.color).toEqual("red");
+
+        // However, the div's style object doesn't reflect any style information
+        // inherited from (in-page or external) CSS style sheets.
+        expect(testDiv.style.fontSize).toEqual("");
+        expect(testDiv.style.border).toEqual("");
+
+        // Modify the CSS border using the element's style property and make sure
+        // the change has been recorded by testing the border width
+        testDiv.style.border = "2px solid red";
+        expect(testDiv.style.borderWidth).toEqual("2px");
+
+        // Cleanup
+        document.getElementsByTagName("head")[0].removeChild(styleElem);
+        document.getElementsByTagName("body")[0].removeChild(containerDiv);
+    });
+
+    it('illustrates fetching computed style values', function () {
+        // Create a style element, add some CSS to it and append it to
+        // the head element
+        const css = "div#testDiv { font-size: 20px; border: 1px dashed rgb(0, 255, 0); display:inline; color:yellow; background-color: blue; }";
+        const styleElem = document.createElement("style");
+        styleElem.appendChild(document.createTextNode(css));
+        document.getElementsByTagName("head")[0].appendChild(styleElem);
+
+        // Create a #testDiv within an invisible container div, define
+        // its 'style' attribute and add the container div to the body
+        // of the page
+        let containerDiv = document.createElement("div");
+        containerDiv.style.display = "none";
+        containerDiv.innerHTML = "<div id='testDiv' style='color:red;'></div>";
+        document.getElementsByTagName("body")[0].appendChild(containerDiv);
+
+        // Obtain the reference to #testDiv div
+        let testDiv = document.getElementById("testDiv");
+        expect(testDiv).not.toBeNull();
+
+        // Use 'getComputedStyle' function to compute the actual style information for
+        // the div element
+        let computedStyles = getComputedStyle(testDiv);
+
+        // CSS color property is specified both in div's style attribute as well as
+        // in the in-page style sheet. Style attribute must take precedence.
+        expect(computedStyles.getPropertyValue("color")).toEqual("rgb(255, 0, 0)");
+
+        // getPropertyValue uses CSS dashed naming for properties instead of the
+        // camel case naming used by JavaScript style object
+        expect(computedStyles.getPropertyValue("font-size")).toEqual("20px");
+        expect(computedStyles.getPropertyValue("display")).toEqual("inline");
+        expect(computedStyles.getPropertyValue("background-color")).toEqual("rgb(0, 0, 255)");
+
+        expect(computedStyles.getPropertyValue("border-style")).toEqual("dashed");
+        expect(computedStyles.getPropertyValue("border-color")).toEqual("rgb(0, 255, 0)");
+    });
 });
