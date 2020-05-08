17ed8abe0a0ecc617d5090dc1d2a31396c58ace1
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2020-05-08 14:15:11 +0200

    HTTPS enabled in webdev/projects/03_socialnetwork
    - Both HTTP and HTTPS servers are run
    - express-enforces-ssl module is used to redirect HTTP
      requests to HTTPS site
    - Helmet module is used to set HSTS header in response requiring
      clients to keep using HTTPS for 2 years (even if client requests
      the HTTP version of the site)
    - Adding server private RSA key and a self-signed TLS certificate (used
      for testing purposes only)

--- webdev/projects/03_socialnetwork/socialnetwork.js
+++ webdev/projects/03_socialnetwork/socialnetwork.js
@@ -6,6 +6,9 @@
 require('module-alias/register');
 
 const express = require("express");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const dbSetup = require("@root/db_setup");
 const session = require("express-session");
@@ -17,16 +20,27 @@ const flash = require("express-flash");
 const passport = require("passport");
 const routes = require("@root/routes");
 const setupPassport = require("@root/passport_setup");
+const enforceTLS = require("express-enforces-ssl");
+const helmet = require("helmet");
+const ms = require("ms");
 
-// Functions that resolve/reject the exported promise, once server starts
-// listening or a failure occurs
-let resolveServerListenPromise = undefined;
-let rejectServerListenPromise = undefined;
+// Functions that resolve/reject the exported promises, once the HTTP
+// and HTTPS servers start listening or a failure occurs
+let resolveHttpServerPromise = undefined;
+let rejectHttpServerPromise = undefined;
+let resolveSecureHttpServerPromise = undefined;
+let rejectSecureHttpServerPromise = undefined;
 
-// Export the promise that is resolved once the server starts listening
-module.exports = new Promise((resolve, reject) => {
-    resolveServerListenPromise = resolve;
-    rejectServerListenPromise = reject;
+// Export a promise that resolves with the HTTP server
+module.exports.httpServerPromise = new Promise((resolve, reject) => {
+    resolveHttpServerPromise = resolve;
+    rejectHttpServerPromise = reject;
+});
+
+// Export a promise that resolves with the HTTPS server
+module.exports.httpsServerPromise = new Promise((resolve, reject) => {
+    resolveSecureHttpServerPromise = resolve;
+    rejectSecureHttpServerPromise = reject;
 });
 
 (async () => {
@@ -35,8 +49,9 @@ module.exports = new Promise((resolve, reject) => {
         await dbSetup.connect();
     }
     catch (err) {
-        // Reject the exported promise and re-throw
-        rejectServerListenPromise();
+        // Reject the exported promises and re-throw
+        rejectHttpServerPromise();
+        rejectSecureHttpServerPromise();
         throw err;
     }
 
@@ -56,6 +71,21 @@ module.exports = new Promise((resolve, reject) => {
 
     // Add Morgan logging middleware
     app.use(logger("dev"));
+
+    // Add middleware that redirects to HTTPS URL whenever an HTTP
+    // request is received.
+    // TODO:
+    //  should be used for testing too, once integration tests
+    //  are fixed to work with HTTPS.
+    if (process.env.NODE_ENV !== "test") {
+        app.use(enforceTLS());
+    }
+
+    // Use HSTS to instruct client to stay on HTTPS for 2 years
+    app.use(helmet.hsts({
+        maxAge: ms("2 years"),
+        includeSubDomains: true
+    }));
 
     // Add express.static middleware that will serve static assets
     // from the public/ directory
@@ -97,16 +127,31 @@ module.exports = new Promise((resolve, reject) => {
     // Register the custom Router
     app.use(routes);
 
-    // Start the HTTP server at port 3000
-    const httpServer = app.listen(3000, () => {
-        console.log("Server started at port 3000");
+    // Start the HTTP server
+    const httpServer = http.createServer(app).listen(3080, "localhost", () => {
+        console.log(`HTTP server started at port ${httpServer.address().port}`);
 
         // Resolve the exported promise with the HTTP server instance
-        resolveServerListenPromise(httpServer);
+        resolveHttpServerPromise(httpServer);
     });
 
-    // Attach a listener for the server's `close` event. Once the server
-    // is closed the database connection is terminated as well
+    // Start the HTTPS server
+    const httpsServer = https.createServer({
+        key: fs.readFileSync(path.join(__dirname, "tls_cert", "socialnetwork-key.pem")),
+        cert: fs.readFileSync(path.join(__dirname, "tls_cert", "socialnetwork-cert.pem"))
+    },
+    app)
+        .listen(3443, "localhost", () => {
+            console.log(`HTTPS server started at port ${httpsServer.address().port}`);
+
+            // Resolve the exported promise with the HTTP server instance
+            resolveSecureHttpServerPromise(httpsServer);
+        });
+
+    // Attach a listener for the HTTP server's `close` event. Once the server
+    // is closed the database connection is terminated as well. An assumption
+    // is made that HTTPS server will never remain opened after HTTP server
+    // is closed
     httpServer.on("close", () => {
         dbSetup.close();
     });
