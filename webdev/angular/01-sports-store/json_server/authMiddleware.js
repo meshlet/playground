9d2f2a9ebe576dd79c36173d2068ecc7fc634f1c
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2020-10-04 23:39:18 +0200

    Adding features to webdev/angular/01-sports-store project

---
+++ webdev/angular/01-sports-store/json_server/authMiddleware.js
@@ -0,0 +1,55 @@
+/**
+ * Simple authentication middleware for json-server used only
+ * for testing purposes.
+ */
+const jwt = require("jsonwebtoken");
+
+const APP_SECRET = "sports_store_app_secret";
+const USERNAME = "admin";
+const PASSWORD = "nimda";
+
+module.exports =(req, res, next) => {
+  if ((req.url === "/api/login" || req.url === "/login") && req.method === "POST") {
+    // The user is attempting to gain administrator privileges
+    if (req.body != null && req.body.name === USERNAME && req.body.password === PASSWORD) {
+      // Create a signed JWT token and return it to the client
+      const token = jwt.sign({ data: USERNAME, expiresIn: "1h"}, APP_SECRET);
+      res.json({ success: true, token: token });
+    }
+    else {
+      res.json({ success: false });
+      res.end();
+      return;
+    }
+  }
+  else if (((req.url === "/api/products" || req.url === "/products" ||
+            req.url === "/api/categories" || req.url === "/categories") && req.method !== "GET") ||
+           (req.url.startsWith("/orders") && req.method !== "POST")) {
+    // This is either:
+    // 1) A request that attempts to update the products or categories
+    // 2) or a request that attempts to update existing orders (as opposed
+    //    to registering a new order)
+    //
+    // both of which require admin access rights. Hence, try fetching the
+    // JWT token from the headers and verify it
+    let token = req.headers["authorization"];
+    if (token != null && token.startsWith("Bearer<")) {
+      token = token.substring(7, token.length - 1);
+      try {
+        // Exception is thrown if token cannot be verified
+        jwt.verify(token, APP_SECRET);
+        next();
+        return;
+      }
+      catch(err) {}
+
+      // JWT verification failed, report 401 Unauthorized error
+      res.statusCode = 401;
+      res.end();
+      return;
+    }
+  }
+
+  // Proceed to next middleware
+  next();
+};
---
+++ webdev/angular/01-sports-store/src/model/static.datasource.ts
@@ -0,0 +1,79 @@
+/**
+ * Used as a data source during development only. This class will
+ * eventually be removed and data retrieved from an HTTP server.
+ */
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { Observable, from } from 'rxjs';
+
+@Injectable()
+export class StaticDatasource {
+  private products: Product[] = [
+    new Product(
+      1,
+      'Adidas sneakers',
+      'soccer',
+      'Adidas soccer sneakers',
+      175),
+
+    new Product(
+      2,
+      'Lifejacket',
+      'Watersports',
+      'Protective and fashionable',
+      48.95),
+
+    new Product(
+      3,
+      'Soccer Ball',
+      'Soccer',
+      'FIFA-approved size and weight',
+      19.50),
+
+    new Product(
+      4,
+      'Corner Flags',
+      'Soccer',
+      'Give your playing field a professional touch',
+      34.95),
+
+    new Product(
+      5,
+      'Stadium',
+      'Soccer',
+      'Flat-packed 35,000-seat stadium',
+      79500),
+
+    new Product(
+      6,
+      'Thinking Cap',
+      'Chess',
+      'Improve brain efficiency by 75%',
+      16),
+
+    new Product(
+      7,
+      'Unsteady Chair',
+      'Chess',
+      'Secretly give your opponent a disadvantage',
+      29.95),
+
+    new Product(
+      8,
+      'Human Chess Board',
+      'Chess',
+      'A fun game for the family',
+      75),
+
+    new Product(
+      9,
+      'Bling Bling King',
+      'Chess',
+      'Gold-plated, diamond-studded King',
+      1200)
+  ];
+
+  getProducts(): Observable<Product[]> {
+    return from([this.products]);
+  }
+}
