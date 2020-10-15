4e1daab3e5c0afd173c3709a0789f48ce3497183
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2020-10-15 23:35:35 +0200

    Extending webdev/angular/01-sports-store
    - Adding OrderRepository class for managing Orders
    - StaticDatasource extended with a saveOrder method

--- webdev/angular/01-sports-store/src/app/model/model.module.ts
+++ webdev/angular/01-sports-store/src/app/model/model.module.ts
@@ -6,8 +6,11 @@ import { ProductRepository } from './product.repository';
 import { StaticDatasource } from './static.datasource';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
 
 @NgModule({
-  providers: [ProductRepository, StaticDatasource, Cart, Order]
+  providers: [
+    ProductRepository, StaticDatasource, Cart, Order, OrderRepository
+  ]
 })
 export class ModelModule {}
---
+++ webdev/angular/01-sports-store/src/app/model/order.repository.ts
@@ -0,0 +1,22 @@
+/**
+ * Repository for managing Order objects.
+ */
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from './order.model';
+import { StaticDatasource } from './static.datasource';
+
+@Injectable()
+export class OrderRepository {
+  private orders: Order[] = [];
+
+  constructor(private dataSource: StaticDatasource) {}
+
+  get Orders(): Order[] {
+    return this.orders;
+  }
+
+  saveOrder(order: Order): Observable<Order> {
+    return this.dataSource.saveOrder(order);
+  }
+}
