10c1deba7c89255fb50eee50b0864c8a91968ceb
Dzanan Bajgoric <dzanan.bajgoric@gmail.com>
2022-06-21 01:10:13 +0200

    Changes in webdev/mean/loc8r/loc8r-spa
    - modules/loc8r-ng/src/app/dal/rest.datasource.ts
      has been written with assumption that HttpClient
      invokes the next callback for any server response
      regardless of the status code, while error callback
      is invoked only if client can't reach the server.
      However, this is not the case. HttpClient invokes
      the next callback only for responses whose status
      code is < 400. For >= 400 status codes and in case
      of any other errors HttpClient invokes the error
      callback. The code has been refactores to account
      for this and correctly process Http errors.

--- webdev/mean/loc8r/loc8r-spa/modules/loc8r-ng/src/app/dal/rest.datasource.ts
+++ webdev/mean/loc8r/loc8r-spa/modules/loc8r-ng/src/app/dal/rest.datasource.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import {
   GetLocationsRspI,
   GetOneLocationRspI,
@@ -21,31 +21,32 @@ import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 /**
- * Sanity checks REST API response to make sure its format is as
- * expected and makes sure that response type matches the type
+ * Makes sure that HTTP response body corresponds to a successfull
+ * REST server response and that reponse type matches the type
  * specified by the caller. If these two checks pass, delegates
- * processing of the success/failure response to the success/failure
- * callback.
+ * processing of the response to the callback.
+ *
+ * @note This function can only be used with successfull REST
+ * responses. For processing REST server errors see
+ * processRestRspError function.
  *
  * @note The overload is used to map `expectedSuccessRspType` literal
  * to a SuccessRspUnionT type, indicating the type of response passed
- * to the `handleSuccessRspCb` callback (if REST API response is a
- * successfull one).
+ * to the `handleSuccessRspCb` callback.
  */
-function processRestResponse<SuccessRspTypeLiteralT extends SuccessRspTypeLiteralsT>(
+function processRestRspSuccess<SuccessRspTypeLiteralT extends SuccessRspTypeLiteralsT>(
   restApiRes: HttpResponse<unknown>,
   expectedSuccessRspType: SuccessRspTypeLiteralT,
-  handleSuccessRspCb: (restRspBody: SuccessRspTypeLiteralToType<SuccessRspTypeLiteralT>) => void,
-  handleFailureRspCb: (error: RestErrorI) => void): void;
-function processRestResponse(
+  handleSuccessRspCb: (restRspBody: SuccessRspTypeLiteralToType<SuccessRspTypeLiteralT>) => void): void;
+function processRestRspSuccess(
   restApiRes: HttpResponse<unknown>,
   expectedSuccessRspType: SuccessRspTypeLiteralsT,
-  handleSuccessRspCb: (restRspBody: SuccessRspUnionT) => void,
-  handleFailureRspCb: (error: RestErrorI) => void) {
+  handleSuccessRspCb: (restRspBody: SuccessRspUnionT) => void) {
   if (!isValidRestResponse(restApiRes.body)) {
     // Unexpected response received from the REST server
     throw new FrontendError(ErrorCode.InternalServerError);
   }
+
   const json = restApiRes.body;
   if (json.success) {
     if (json.body.type !== expectedSuccessRspType) {
@@ -55,7 +56,54 @@ function processRestResponse(
     handleSuccessRspCb(json.body);
   }
   else {
-    handleFailureRspCb(json.error);
+    // REST server should never send an error response with < 400 HTTP status
+    // (HttpClient resolves with HttpResponse only for requests completed with
+    // non-error status codes i.e. < 400. Otherwise, HttpErrorResponse is
+    // generated).
+    throw new FrontendError(ErrorCode.InternalServerError);
+  }
+}
+
+/**
+ * Process HTTP failure response.
+ *
+ * This function makes sure that response is of HttpErrorResponse type.
+ * Furthermore, if response body (i.e. rsp.error) is a valid REST server
+ * response, it makes sure that it is a failure and not a successfull
+ * REST response. If both these conditions are true a callback with
+ * both original failure response and type-checked response body is
+ * called. If response body is not a well-formed REST response, a
+ * NetworkError is reported as this can only happen if HttpClient
+ * couldn't reach the REST server.
+ *
+ * @note This function can only be used with failure HTTP responses.
+ * For processing successfull responses see processRestRspSuccess.
+ */
+function processRestRspError(rsp: unknown,
+                             errCb: (err: RestErrorI, errRsp: HttpErrorResponse) => void) {
+  if (rsp instanceof HttpErrorResponse) {
+    if (!isValidRestResponse(rsp.error)) {
+      // This should happen only if HttpClient wasn't able to connect to
+      // the REST API in which case HttpErrorResponse.error would not have
+      // been set by the REST server.
+      throw new FrontendError(ErrorCode.NetworkError);
+    }
+
+    const json = rsp.error;
+    if (json.success) {
+      // REST server should never send a success response with >= 400 HTTP status
+      // (HttpClient resolves with HttpErrorResponse only for requests completed with
+      // error status codes i.e. >= 400. Otherwise, HttpResponse is generated).
+      throw new FrontendError(ErrorCode.InternalServerError);
+    }
+
+    // Invoke callback with the REST API error
+    errCb(json.error, rsp);
+  }
+  else {
+    // Something has gone quite wrong if HttpClient reports an error
+    // whose type is not HttpErrorResponse
+    throw new FrontendError(ErrorCode.InternalServerError);
   }
 }
 
@@ -75,33 +123,40 @@ export class RestDataSource implements BaseDataSource {
           observe: 'response',
           params: { longitude: longitude, latitude: latitude, maxDistance: maxDistance }
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'GetLocations',
-              (json: GetLocationsRspI) => {
-                subscriber.next(json.locations);
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'GetLocations',
+                (json: GetLocationsRspI) => {
+                  subscriber.next(json.locations);
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 
@@ -112,33 +167,40 @@ export class RestDataSource implements BaseDataSource {
         {
           observe: 'response'
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'GetOneLocation',
-              (json: GetOneLocationRspI) => {
-                subscriber.next(json.location);
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'GetOneLocation',
+                (json: GetOneLocationRspI) => {
+                  subscriber.next(json.location);
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 
@@ -149,33 +211,40 @@ export class RestDataSource implements BaseDataSource {
         {
           observe: 'response'
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'GetOneReview',
-              (json: GetOneReviewRspI) => {
-                subscriber.next(json.review);
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'GetOneReview',
+                (json: GetOneReviewRspI) => {
+                  subscriber.next(json.review);
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 
@@ -187,39 +256,46 @@ export class RestDataSource implements BaseDataSource {
         {
           observe: 'response'
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'CreateReview',
-              (json: CreateReviewRspI) => {
-                subscriber.next(json.review);
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status === 401) {
-                  subscriber.error(new FrontendError(ErrorCode.Unauthenticated, err.message));
-                }
-                else if (rsp.status === 404) {
-                  subscriber.error(new FrontendError(ErrorCode.ResourceNotFound));
-                }
-                else if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'CreateReview',
+                (json: CreateReviewRspI) => {
+                  subscriber.next(json.review);
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status === 401) {
+                    subscriber.error(new FrontendError(ErrorCode.Unauthenticated, err.message));
+                  }
+                  else if (httpErrRsp.status === 404) {
+                    subscriber.error(new FrontendError(ErrorCode.ResourceNotFound));
+                  }
+                  else if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 
@@ -231,36 +307,43 @@ export class RestDataSource implements BaseDataSource {
         {
           observe: 'response'
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'CreateUser',
-              (json: CreateUserRspI) => {
-                subscriber.next(json.user);
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status === 403) {
-                  subscriber.error(new FrontendError(ErrorCode.Forbidden, err.message));
-                }
-                else if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'CreateUser',
+                (json: CreateUserRspI) => {
+                  subscriber.next(json.user);
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status === 403) {
+                    subscriber.error(new FrontendError(ErrorCode.Forbidden, err.message));
+                  }
+                  else if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 
@@ -272,36 +355,42 @@ export class RestDataSource implements BaseDataSource {
         {
           observe: 'response'
         })
-        .subscribe(rsp => {
-          try {
-            processRestResponse(
-              rsp,
-              'LoginUser',
-              (json: LoginUserRspI) => {
-                subscriber.next({ user: json.user, jwt: json.jwt });
-                subscriber.complete();
-              },
-              (err: RestErrorI) => {
-                if (rsp.status === 401) {
-                  subscriber.error(new FrontendError(ErrorCode.Unauthenticated, err.message));
-                }
-                else if (rsp.status < 500) {
-                  subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
-                }
-                else {
-                  subscriber.error(new FrontendError(ErrorCode.InternalServerError));
-                }
-              });
-          }
-          catch (exception) {
-            subscriber.error(exception);
-          }
-        },
-        err => {
-          // This can only fail due to a network error.
-          console.log(err);
-          subscriber.error(new FrontendError(ErrorCode.NetworkError));
-        });
+        .subscribe(
+          rsp => {
+            try {
+              processRestRspSuccess(
+                rsp,
+                'LoginUser',
+                (json: LoginUserRspI) => {
+                  subscriber.next({ user: json.user, jwt: json.jwt });
+                  subscriber.complete();
+                });
+            }
+            catch (exception) {
+              subscriber.error(exception);
+            }
+          },
+          errRsp => {
+            try {
+              processRestRspError(
+                errRsp,
+                (err, httpErrRsp) => {
+                  if (httpErrRsp.status === 401) {
+                    subscriber.error(new FrontendError(ErrorCode.Unauthenticated, err.message));
+                  }
+                  else if (httpErrRsp.status < 500) {
+                    subscriber.error(new FrontendError(ErrorCode.BadRequest, err.message));
+                  }
+                  else {
+                    subscriber.error(new FrontendError(ErrorCode.InternalServerError));
+                  }
+                });
+            }
+            catch (exception) {
+              console.log(exception);
+              subscriber.error(exception);
+            }
+          });
     });
   }
 }
